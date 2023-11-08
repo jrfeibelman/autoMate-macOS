4601ded07b2c124de178b19e8276108d604c1964
Jason Feibelman <jason.feibelman@gmail.com>
2023-11-08 11:46:51 -0500

    Initial Commit

---
+++ autoMate/autoMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  autoMateApp.swift
+//  autoMate
+//
+//  Created by JR on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct autoMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
